1f2eef7b8ca478d7eb6c9cee7b1ca355a82bdafc
Boran Seckin <boran@boranseckin.com>
2024-11-13 19:05:26 -0500

    fix: cleanup

--- examples/default_dev_eui/main.c
+++ examples/default_dev_eui/main.c
@@ -39,11 +39,7 @@ static char event_str[128];
 void gpio_event_string(char *buf, uint32_t events);
 
 void gpio_callback(uint gpio, uint32_t events) {
-  // Put the GPIO event(s) that just happened into event_str so we can print it
-  // gpio_event_string(event_str, events);
-  // printf("GPIO %d %s\n", gpio, event_str);
   printf("IRQ\n");
-
   uint16_t irq_status = SX126xGetIrqStatus();
   printf("%d\n", irq_status);
 }
@@ -81,8 +77,6 @@ int main(void) {
 
   SX126xIoInit();
 
-  // a
-  // SX126xIoIrqInit(*DioIrq);
   gpio_set_irq_enabled_with_callback(10, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true,
                                      &gpio_callback);
 
@@ -160,32 +154,4 @@ int main(void) {
   }
 
   return 0;
-}
-
-static const char *gpio_irq_str[] = {
-    "LEVEL_LOW",  // 0x1
-    "LEVEL_HIGH", // 0x2
-    "EDGE_FALL",  // 0x4
-    "EDGE_RISE"   // 0x8
-};
-
-void gpio_event_string(char *buf, uint32_t events) {
-  for (uint i = 0; i < 4; i++) {
-    uint mask = (1 << i);
-    if (events & mask) {
-      // Copy this event string into the user string
-      const char *event_str = gpio_irq_str[i];
-      while (*event_str != '\0') {
-        *buf++ = *event_str++;
-      }
-      events &= ~mask;
-
-      // If more events add ", "
-      if (events) {
-        *buf++ = ',';
-        *buf++ = ' ';
-      }
-    }
-  }
-  *buf++ = '\0';
 }
